b6d06ebcd4e8cad6c1cf6dd234394a1dcfa884bd
sergio williams <wllyvn@gmail.com>
2023-10-19 08:25:07 -0300

    init

---
+++ app_sli.py
@@ -0,0 +1,47 @@
+from flask import Flask, request , jsonify
+import requests
+from json import loads
+
+host='0.0.0.0'
+host_ldap='10.253.251.16'
+
+em_manutenção=False
+
+def create_app():
+    app=Flask(__name__)
+
+    @app.before_request
+    def log_request_info():
+        data=request.data
+        url=request.url.replace(host,host_ldap)
+        head=dict(request.headers)
+        if em_manutenção:
+            return jsonify({'response':False, 'mensg':'serviço em manutenção'})
+
+        if request.method=="POST":
+            data=request.data
+            if data:
+                data=loads(data)
+            else:
+                data={}
+            print(data)
+            r=requests.post(url,headers=head , json=data)
+            print(r.text)
+            return r.text
+        
+        elif request.method=='OPTIONS':
+            r=requests.options(url,headers=head)
+            print(r.text)
+            return r.text
+            
+        elif request.method=='GET':
+            r=requests.get(url,headers=head)
+            return r.text
+
+    return app
+
+
+if __name__=='__main__':
+    app=create_app()
+    app.run(port=5001, host=host, debug=1)
+
